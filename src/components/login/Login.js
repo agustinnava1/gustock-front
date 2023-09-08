6ea23713d02a0f623c248c9d57910919ef00f8a1
agustinnava0 <agustinnava0@gmail.com>
2023-09-08 18:24:05 -0300

    login arreglo

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 
+import './login.css'
+
 const Login = () => {
 
 	const[userDetail, setUserDetail] = useState({
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
